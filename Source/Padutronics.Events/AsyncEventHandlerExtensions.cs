9729866c3855ac496b901eb80a200a112aae8871
Pavel Dubsky <pavel.dubsky@padutronics.com>
2023-03-23 12:44:01 +0100

    Add extension method Padutronics.Events.AsyncEventHandler1.InvokeAllAsync

--- Source/Padutronics.Events/AsyncEventHandlerExtensions.cs
+++ Source/Padutronics.Events/AsyncEventHandlerExtensions.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Threading.Tasks;
 
 namespace Padutronics.Events;
 
@@ -10,5 +11,11 @@ public static class AsyncEventHandlerExtensions
         where TEventArgs : EventArgs
     {
         return @this.GetInvocationList().Cast<AsyncEventHandler<TEventArgs>>();
+    }
+
+    public static Task InvokeAllAsync<TEventArgs>(this AsyncEventHandler<TEventArgs> @this, object? sender, TEventArgs e)
+        where TEventArgs : EventArgs
+    {
+        return Task.WhenAll(GetHandlers(@this).Select(handler => handler(sender, e)));
     }
 }
