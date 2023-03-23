f93a1247b86201b2130c44b4de162f1533ac5775
Pavel Dubsky <pavel.dubsky@padutronics.com>
2023-03-23 12:43:44 +0100

    Add extension method Padutronics.Events.AsyncEventHandler1.GetHandlers

---
+++ Source/Padutronics.Events/AsyncEventHandlerExtensions.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Padutronics.Events;
+
+public static class AsyncEventHandlerExtensions
+{
+    public static IEnumerable<AsyncEventHandler<TEventArgs>> GetHandlers<TEventArgs>(this AsyncEventHandler<TEventArgs> @this)
+        where TEventArgs : EventArgs
+    {
+        return @this.GetInvocationList().Cast<AsyncEventHandler<TEventArgs>>();
+    }
+}
